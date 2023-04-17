35b81b66660446860c143c0a6c3582a20b62c230
willuhmjs <willuhmjs@gmail.com>
2023-04-16 22:27:44 -0400

    feat: checkpoint for troubleshooting

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,25 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import { onMount } from "svelte";
+    let publicKeyBase64, privateKeyBase64;
+   onMount(async () => {
+     // Generate key pair
+const { publicKey, privateKey } = await window.crypto.subtle.generateKey(
+  {
+    name: "RSA-OAEP",
+    modulusLength: 2048,
+    publicExponent: new Uint8Array([1, 0, 1]),
+    hash: "SHA-256",
+  },
+  true,
+  ["encrypt", "decrypt"]
+);
+
+// Encode key pair in base64
+publicKeyBase64 = window.btoa(String.fromCharCode.apply(null, new Uint8Array(await window.crypto.subtle.exportKey("spki", publicKey))));
+privateKeyBase64 = window.btoa(String.fromCharCode.apply(null, new Uint8Array(await window.crypto.subtle.exportKey("pkcs8", privateKey))));
+})
+</script>
+{#if publicKeyBase64 && privateKeyBase64}
+    <p>Public key: {publicKeyBase64}</p>
+    <p>Private key: {privateKeyBase64}</p>
+{/if}
--- src/routes/database/+server.ts
+++ src/routes/database/+server.ts
@@ -1,18 +1,25 @@
 import { error } from '@sveltejs/kit';
- 
+import crypto from 'crypto';
+
 /** @type {import('./$types').RequestHandler} */
 export const POST = async (RequestEvent) => {
-    const { data, publicKey } = await RequestEvent.request.json()
-    if (!data || !publicKey) throw error(400, "Missing data or publicKey fields!");
+	const { data, publicKey } = await RequestEvent.request.json();
+	if (!data || !publicKey) throw error(400, 'Missing data or publicKey fields!');
 
-    const dataBuffer = new TextEncoder().encode(data);
-    const encryptedDataBuffer = await window.crypto.subtle.encrypt(
-        { name: "RSA-OAEP" },
-        publicKey,
-        dataBuffer
-    );
+	function encryptData(data: string, publicKey: string) {
+		const publicKeyBuffer = Buffer.from(publicKey, 'base64');
+		const encryptedData = crypto.publicEncrypt(
+			{
+				key: publicKeyBuffer,
+				padding: crypto.constants.RSA_PKCS1_OAEP_PADDING,
+				oaepHash: 'sha256'
+			},
+			Buffer.from(data)
+		);
+		return encryptedData.toString('base64');
+	}
 
-    const encryptedData = window.btoa(String.fromCharCode(...new Uint8Array(encryptedDataBuffer)));
+	const encryptedData = encryptData(data, publicKey);
 
-    return new Response(`${encryptedData}`);
+	return new Response(`${encryptedData}`);
 };
