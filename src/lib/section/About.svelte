8c850fe38396e880d30e9ed3700501c69a9f57d0
willuhmjs <willuhmjs@gmail.com>
2023-04-18 21:52:22 -0400

    feaT: space warning

--- src/lib/section/About.svelte
+++ src/lib/section/About.svelte
@@ -9,7 +9,7 @@
 		<div class="subArea">
 			<h3>Description:</h3>
 			<p>This website is an open-source demonstration and implementation of a public RSA encrypted key-value database. Data is identified by its public key and decrypted on the client side using its private key. Try it out by using the generated (or pre-existing) key pair to encrypt and decrypt data on the database.</p>
-			<Callout type="warning">All data, while encrypted, is available to anyone with the public key. All uploaded data is automatically deleted after 24 hours.</Callout>
+			<Callout type="warning">All data, while encrypted, is available to anyone with the public key.<br>All uploaded data is automatically deleted after 24 hours.</Callout>
 		</div>
 	</div>
 </section>
